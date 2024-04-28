c4823081bdc9d7abb73f88b045090c3c5d384d49
ludanekrasova <79700631+ludanekrasova@users.noreply.github.com>
2024-04-28 12:30:50 +0500

    Add files via upload

--- main.py
+++ main.py
@@ -6,9 +6,9 @@ import os
 from model import predict
 
 dtn = datetime.now() + timedelta(hours=5) # Дата
-bot = telebot.TeleBot('123') # Токен бота
+bot = telebot.TeleBot('123')
 
-# в режиме разработки выводится класс и схожесть, в обычном простой диалог
+#в режиме разработки выводится класс и схожесть, в обычном простой диалог
 dev = True
 
 admin = [123] # Тг id куда будут приходить логи
@@ -27,6 +27,7 @@ def log(message, answer_class=None, answer=None, similarity=None):
     else:
         print("------", file=botlogfile)
         print(dtn.strftime('%Y-%m-%d %H:%M:%S'), file=botlogfile)
+#        print("Сообщение от {0} (id = {2}) \n {3}".format(message.from_user.first_name, message.from_user.last_name, str(message.from_user.id), message.text), file=botlogfile)
         print("Сообщение от {0} (id = {1}) \n {2}".format(message.from_user.first_name, str(message.from_user.id), message.text), file=botlogfile)
         if answer_class and answer and similarity:
             print("Ответ: {}\nКласс ответа: {}\nСхожесть: {}".format(answer, answer_class, similarity),
@@ -43,38 +44,37 @@ def get_text_messages(message):
         # строка, которую вводит пользователь
         answer_class, category, answer, similarity = predict(message.text)
 
-        markup = types.InlineKeyboardMarkup()
+        if similarity >= 0.7:
+            if dev == True:
+                bot.send_message(message.from_user.id, f"{answer} \n\n Класс ответа: {answer_class}   {category} \n\n Схожесть: {similarity}", parse_mode="Markdown")
+            else:
+                bot.send_message(message.from_user.id, f"{answer} ", parse_mode="Markdown")
 
-        if (similarity > 0.2) and (similarity < 0.6):
+        elif similarity <= 0.2:
+            bot.send_message(message.from_user.id, f"К сожалению, я не могу ответить на ваш вопрос. Пожалуйста, дождитесь ответа оператора.", parse_mode="Markdown")
+
+        else:
+            markup = types.InlineKeyboardMarkup()
             btn1= types.InlineKeyboardButton(text='Да', callback_data='correct')
             btn2= types.InlineKeyboardButton(text='Нет', callback_data='incorrect')
             markup.add(btn1, btn2)
 
             bot.send_message(message.from_user.id, f"Это вопрос из категории: {category}?", parse_mode="Markdown", reply_markup=markup)
 
-        else:
-            if dev == True:
-                bot.send_message(message.from_user.id, f"{answer} \n\n Класс ответа: {answer_class}   {category} \n\n Схожесть: {similarity}", parse_mode="Markdown", reply_markup=markup)
-            else:
-                bot.send_message(message.from_user.id, f"{answer} ", parse_mode="Markdown", reply_markup=markup)
-
-            log(message, answer_class, answer, similarity)
+        log(message, answer_class, answer, similarity)
 
     # Обработчик нажатия на кнопки
     @bot.callback_query_handler(func=lambda call: True)
     def callback_handler(call):
-        # строка, которую вводит пользователь
-        answer_class, category, answer, similarity = predict(call.message.text)
 
-        markup = types.InlineKeyboardMarkup()
         callback = call.data
         if callback == "correct":
             if dev == True:
-                bot.send_message(call.message.chat.id, f"{answer} \n\n Класс ответа: {answer_class}   {category} \n\n Схожесть: {similarity}", parse_mode="Markdown", reply_markup=markup)
+                bot.send_message(call.message.chat.id, f"{answer} \n\n Класс ответа: {answer_class}   {category} \n\n Схожесть: {similarity}", parse_mode="Markdown")
             else:
-                bot.send_message(call.message.chat.id, f"{answer} ", parse_mode="Markdown", reply_markup=markup)
+                bot.send_message(call.message.chat.id, f"{answer} ", parse_mode="Markdown")
         elif callback == "incorrect":
-            bot.send_message(call.message.chat.id, f"Пожалуйста, сформулируйте вопрос более развернуто.", parse_mode="Markdown", reply_markup=markup)
+            bot.send_message(call.message.chat.id, f"Пожалуйста, сформулируйте вопрос более развернуто.", parse_mode="Markdown")
 
 
 bot.polling(none_stop=True, interval=0)
